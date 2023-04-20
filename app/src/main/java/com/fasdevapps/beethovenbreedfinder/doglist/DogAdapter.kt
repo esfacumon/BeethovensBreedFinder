c7f75e68687b43f2f33466bd6164e23568504c49
fas <facunosierra@usal.es>
2023-04-20 22:36:42 +0200

    Load dog list using recyclerview

---
+++ app/src/main/java/com/fasdevapps/beethovenbreedfinder/doglist/DogAdapter.kt
@@ -0,0 +1,46 @@
+package com.fasdevapps.beethovenbreedfinder.doglist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.fasdevapps.beethovenbreedfinder.databinding.DogListItemBinding
+import com.fasdevapps.beethovenbreedfinder.model.Dog
+
+class DogAdapter: ListAdapter<Dog, DogAdapter.DogViewHolder>(DiffCallback) {
+
+    companion object DiffCallback: DiffUtil.ItemCallback<Dog>() {
+        override fun areItemsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            return oldItem === newItem // check if they are same instance
+        }
+
+
+        override fun areContentsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+
+        val binding = DogListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return DogViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(dogViewHolder: DogViewHolder, position: Int) {
+        val dog = getItem(position)
+        dogViewHolder.bind(dog)
+    }
+
+
+    inner class DogViewHolder(
+        private val binding: DogListItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(dog: Dog) {
+            binding.tvDogName.text = dog.name
+        }
+    }
+}
